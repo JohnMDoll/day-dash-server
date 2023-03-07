150504b5d9d9d3ddcb39a757ba03efc9674936f7
JohnMDoll <johnmdoll3@gmail.com>
2023-03-07 12:37:48 -0600

    user registration imlemented

--- daydash/urls.py
+++ daydash/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from daydashapi.views import login, register
+
+router = routers.DefaultRouter(trailing_slash=False)
+# router.register(r'parks', ParkView, 'park')
 
 urlpatterns = [
+    path('register', register),
+    path('login', login),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
---
+++ daydashapi/views/__init__.py
@@ -0,0 +1,1 @@
+from .auth_view import login, register
