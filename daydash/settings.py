30758ab3f72a83f8ee3af181c9d897d5d44675fc
JohnMDoll <johnmdoll3@gmail.com>
2023-03-15 14:58:13 -0500

    1st attempt at deployment settings

--- daydash/settings.py
+++ daydash/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-
+# from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "False")
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", '').split(",")
+DEVELOPMENT_MODE = os.environ.get("DEVELOPMENT_MODE", "False")
 
 # Application definition
 
