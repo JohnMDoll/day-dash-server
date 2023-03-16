3d876df7a191977e0ae31231d6da6963d63eaee5
JohnMDoll <johnmdoll3@gmail.com>
2023-03-16 11:39:16 -0500

    added cors whitelist back into settings.py

--- daydash/settings.py
+++ daydash/settings.py
@@ -44,10 +44,10 @@ INSTALLED_APPS = [
     'daydashapi',
 ]
 
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:3000',
-#     'http://127.0.0.1:3000'
-# )
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
